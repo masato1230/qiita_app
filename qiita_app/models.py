7e825ebac6be1b0839272bae45f4d1fe203cae5b
Your Name <you@example.com>
2020-10-21 16:20:20 +0900

    create model and update view

--- qiita_app/models.py
+++ qiita_app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Result(models.Model):
-  date = models.DateTimeField
+  date = models.DateTimeField(auto_now=True, null=True, editable=True)
   key = models.CharField(max_length=240)
   value = models.IntegerField()
 
   def __str__(self):
-    return self.key, self.date
+    return (str(self.key) + "   " + str(self.date))
  
--- qiita_app/views.py
+++ qiita_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests
 from collections import Counter
+from .models import Result
 
 
 
@@ -26,4 +27,31 @@ def home(request):
   
   return render(request, 'qiita_app/home.html', {'result': resulut})
 
-  
+def update(request):
+  PER_PAGE = "100"
+  tag_list = []
+  ACCESS_TOKEN = "Bearer f339bc4f73e81b17c450325c0d52c44db52f160a"
+
+  for page in range(1, 10):
+      page = str(page)
+      url = "https://qiita.com/api/v2/items?page=" + page + "&per_page=" + PER_PAGE
+
+      res = requests.get(url, headers={"Authorization": ACCESS_TOKEN}).json()
+      for i in range(int(PER_PAGE)):
+          tags = res[i]['tags']
+          for tag in tags:
+              tag_list.append(tag['name'])
+              print(tag['name'])
+  print(Counter(tag_list))
+  results = Counter(tag_list).most_common(10)
+  print(len(tag_list))
+  print(res[1]['created_at'])
+  print(res[-1]['created_at'])
+
+
+  for result in results:
+    print(result)
+    result = Result(date=res[-1]['created_at'],key=result[0], value=result[1])
+    result.save()
+    
+  return render(request, 'qiita_app/home.html', {'results': results})
