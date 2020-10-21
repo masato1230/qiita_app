0437b57c086980a8f075ea75a99aea0888a11063
Your Name <you@example.com>
2020-10-21 19:20:02 +0900

    create home.css and basic of update

--- qiita_app/views.py
+++ qiita_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import date
 import requests
 from collections import Counter
 from .models import Result
@@ -6,27 +7,14 @@ from .models import Result
 
 
 def home(request):
-  PER_PAGE = "100"
-  tag_list = []
-  ACCESS_TOKEN = "Bearer f339bc4f73e81b17c450325c0d52c44db52f160a"
-
-  for page in range(1, 10):
-      page = str(page)
-      url = "https://qiita.com/api/v2/items?page=" + page + "&per_page=" + PER_PAGE
-
-      res = requests.get(url, headers={"Authorization": ACCESS_TOKEN}).json()
-      for i in range(int(PER_PAGE)):
-          tags = res[i]['tags']
-          for tag in tags:
-              tag_list.append(tag['name'])
-              print(tag['name'])
-      print(res[-1]['created_at'])
-  print(Counter(tag_list))
-  resulut = Counter(tag_list)
-  print(len(tag_list))
+  results = Result.objects.filter(date__date=date.today())[:10]
+  for result in results:
+    print(result.key)
+    print(result.value)
   
-  return render(request, 'qiita_app/home.html', {'result': resulut})
+  return render(request, 'qiita_app/home.html', {'results': results})
 
+# ランキングを更新するためのアクション
 def update(request):
   PER_PAGE = "100"
   tag_list = []
@@ -51,7 +39,7 @@ def update(request):
 
   for result in results:
     print(result)
-    result = Result(date=res[-1]['created_at'],key=result[0], value=result[1])
+    result = Result(key=result[0], value=result[1])
     result.save()
     
   return render(request, 'qiita_app/home.html', {'results': results})
